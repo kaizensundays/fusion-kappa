7746427993d08fa4482621df4a3eef5eefaebdcc
KaizenSundays <kaizensundays@localhost>
2022-03-20 16:31:31 -0400

    [ fusion-kappa-alpha ] Some

---
+++ fusion-kappa-alpha/src/main/java/com/kaizensundays/fusion/kappa/alpha/Some.java
@@ -0,0 +1,9 @@
+package com.kaizensundays.fusion.kappa.alpha;
+
+/**
+ * Created: Sunday 3/20/2022, 4:27 PM Eastern Time
+ *
+ * @author Sergey Chuykov
+ */
+public class Some {
+}
---
+++ fusion-kappa-alpha/src/test/java/com/kaizensundays/fusion/kappa/alpha/SomeTest.java
@@ -0,0 +1,17 @@
+package com.kaizensundays.fusion.kappa.alpha;
+
+import org.junit.Test;
+
+/**
+ * Created: Sunday 3/20/2022, 4:27 PM Eastern Time
+ *
+ * @author Sergey Chuykov
+ */
+public class SomeTest {
+
+    @Test
+    public void test() {
+
+    }
+
+}
