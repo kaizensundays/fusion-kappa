e576349a2f9e3d3a66f482db2edd4e13aa87efd7
KaizenSundays <kaizensundays@localhost>
2022-03-26 12:33:47 -0400

    [ fusion-kappa-beta ]

---
+++ fusion-kappa-beta/src/main/java/com/kaizensundays/fusion/kappa/beta/Some.java
@@ -0,0 +1,9 @@
+package com.kaizensundays.fusion.kappa.beta;
+
+/**
+ * Created: Saturday 3/26/2022, 12:16 PM Eastern Time
+ *
+ * @author Sergey Chuykov
+ */
+public class Some {
+}
---
+++ fusion-kappa-beta/src/test/java/com/kaizensundays/fusion/kappa/beta/SomeTest.java
@@ -0,0 +1,17 @@
+package com.kaizensundays.fusion.kappa.beta;
+
+import org.junit.Test;
+
+/**
+ * Created: Saturday 3/26/2022, 12:16 PM Eastern Time
+ *
+ * @author Sergey Chuykov
+ */
+public class SomeTest {
+
+    @Test
+    public void test() {
+
+    }
+
+}
